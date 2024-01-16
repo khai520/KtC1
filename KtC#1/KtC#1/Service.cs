f1fe18c88998ae0c1e010cd99cdfe68aa7e17571
khai520 <45537943+khai520@users.noreply.github.com>
2024-01-16 11:24:01 +0700

    Add files via upload

--- KtC#1/KtC#1/Service.cs
+++ KtC#1/KtC#1/Service.cs
@@ -8,30 +8,48 @@ namespace KtC_1
 {
     internal class Service
     {
-        string check = "0";
+        string check = "1";
         List<SinhVien> list = new List<SinhVien>();
         public void Nhap()
         {
             do
             {
-                Console.Write("Nhập Mã Sv:");
-                string MSv  = Console.ReadLine();
-                Console.Write("Nhập Tên Sv:");
-                string Ten = Console.ReadLine();
-                Console.Write("Nhập năm sinh:");
-                int NS = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Nhập điểm:");
-                double diem = Convert.ToDouble(Console.ReadLine());
-                list.Add(new SinhVien()
+                try
                 {
-                    MaSv1 = MSv,
-                    Ten1 = Ten,
-                    NamSinh1 = NS,
-                    Diem1 = diem
-                });
-                Console.Write("Bạn có muốn nhập tiếp không (0:no / 1:yes):");
-                check = Console.ReadLine();
-            } while (check == "1" || check.ToLower() == "y" );
+                    string MSv = Console.ReadLine();
+                    Console.Write("Nhập Tên Sv:");
+                    string Ten = Console.ReadLine();
+                    Console.Write("Nhập năm sinh:");
+                    int NS = Convert.ToInt32(Console.ReadLine());
+                    if (DateTime.Now.Year - NS < 18 || NS - DateTime.Now.Year > 0)
+                    {
+                        Console.WriteLine("Lỗi năm sinh");
+                        continue;
+                    }
+                    Console.Write("Nhập điểm:");
+                    double diem = Convert.ToDouble(Console.ReadLine());
+                    if (diem > 10 || diem < 0)
+                    {
+                        Console.WriteLine("Lỗi điểm");
+                        continue;
+                    }
+                    list.Add(new SinhVien()
+                    {
+                        MaSv1 = MSv,
+                        Ten1 = Ten,
+                        NamSinh1 = NS,
+                        Diem1 = diem
+                    });
+                    Console.Write("Bạn có muốn nhập tiếp không (0:no / 1:yes):");
+                    check = Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sai kiểu dữ liệu");
+                    continue;
+                }
+                Console.Write("Nhập Mã Sv:");
+            } while (check == "1" || check.ToLower() == "y");
         }
         public void Xuat1()
         {
@@ -44,7 +62,7 @@ namespace KtC_1
         {
             foreach(var item in list)
             {
-                if (item.Diem1 >= 8)
+                if (item.Diem1 >= 8 && item.Diem1 < 25)
                 {
                     item.inThongTin();
                 }
@@ -54,10 +72,10 @@ namespace KtC_1
         {
             for (int i = 0; i < list.Count(); i++)
             {
-                if (2023 - list[i].NamSinh1 > 25)
+                if (DateTime.Now.Year - list[i].NamSinh1 > 25 && list[i].Diem1 <= 8)
                 {
                     list.Remove(list[i]);
-                    i--;
+                    i--; 
                 }
             }
         }
